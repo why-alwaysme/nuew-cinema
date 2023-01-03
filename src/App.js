2dc3ef8a55cc7239e130492da8c06a57ddd64820
whyalwaysme <coworasavanila@gmail.com>
2023-01-03 19:37:08 +0700

    Update 1 - Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import './App.css';
+import {getTvList, searchTv} from "./Api";
+import {
+    Divider, Text, SimpleGrid,
+    Card, CardHeader, CardBody, 
+    Heading,
+    Input, Image,
+    ChakraProvider,
+  } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const [popularTv, setPopularTv] = useState([])
+  
+  useEffect(() => {
+    getTvList().then((result) => {
+      setPopularTv(result)
+    })
+  }, [])
+
+  const PopularTvList = () => {
+    return popularTv.map((tv, i) => {
+      return(
+        <Card key={i} align='center'>
+          <CardHeader>
+              <Heading size='md'>{tv.original_name}</Heading>
+          </CardHeader>
+          <CardBody>
+            <Image objectfit='cover' src={`${process.env.REACT_APP_BASEIMGURL}/${tv.poster_path}`} alt=".." />
+          </CardBody>
+          <Divider />
+              <Text as='i' >{tv.first_air_date}</Text>
+              <Text marginBottom={3} color='orange' fontsize='2xl' as='b' >{tv.vote_average}</Text>
+        </Card>
+      )
+    } )
+  }
+
+  const search = async (q) => {
+    if(q.length > 3){
+      const query = await searchTv(q)
+      setPopularTv(query.results)
+    }
+  }
+
+  console.log({ popularTv: popularTv })
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>NUEW CINEMA</h1>
+
+          <ChakraProvider>
+          <Heading color='teal' >NUEW CINEMA</Heading>
+            <Input className="tv-search" htmlSize={70} width="auto" variant='flushed' placeholder='Cari Series..' _placeholder={{ color: '#fff'}} onChange={({ target }) => search(target.value)}/>
+              <SimpleGrid marginBottom={10} spacing={4} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
+                <PopularTvList/>
+              </SimpleGrid>
+          </ChakraProvider>
+
       </header>
     </div>
   );
