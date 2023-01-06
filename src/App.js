a7a26f69daf0a453d4f89bbad9cc85305d523ffa
whyalwaysme <coworasavanila@gmail.com>
2023-01-06 23:07:03 +0700

    Update 2 - Skeleton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {getTvList, searchTv} from "./Api";
 import {
     Divider, Text, SimpleGrid,
     Card, CardHeader, CardBody, 
-    Heading,
+    Heading, Skeleton,
     Input, Image,
     ChakraProvider,
   } from '@chakra-ui/react';
@@ -23,15 +23,23 @@ const App = () => {
     return popularTv.map((tv, i) => {
       return(
         <Card key={i} align='center'>
+          <Skeleton isLoaded>
           <CardHeader>
               <Heading size='md'>{tv.original_name}</Heading>
           </CardHeader>
+          </Skeleton>
+          <Skeleton isLoaded>
           <CardBody>
             <Image objectfit='cover' src={`${process.env.REACT_APP_BASEIMGURL}/${tv.poster_path}`} alt=".." />
           </CardBody>
+          </Skeleton>
           <Divider />
+          <Skeleton isLoaded>
               <Text as='i' >{tv.first_air_date}</Text>
+          </Skeleton>
+          <Skeleton isLoaded>
               <Text marginBottom={3} color='orange' fontsize='2xl' as='b' >{tv.vote_average}</Text>
+          </Skeleton>
         </Card>
       )
     } )
@@ -51,7 +59,9 @@ const App = () => {
       <header className="App-header">
 
           <ChakraProvider>
-          <Heading color='teal' >NUEW CINEMA</Heading>
+            <Skeleton isLoaded startColor='pink.500' endColor='orange.500' height='50px'>
+              <Heading color='teal' >NUEW CINEMA</Heading>
+            </Skeleton>
             <Input className="tv-search" htmlSize={70} width="auto" variant='flushed' placeholder='Cari Series..' _placeholder={{ color: '#fff'}} onChange={({ target }) => search(target.value)}/>
               <SimpleGrid marginBottom={10} spacing={4} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
                 <PopularTvList/>
